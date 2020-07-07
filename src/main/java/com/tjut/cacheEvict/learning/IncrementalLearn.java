036e7ee3165adcd1644386efe6c980b5f9452412
cv <botang2017@gmail.com>
2020-07-07 16:43:32 +0800

    framwork debug

--- src/main/java/com/tjut/cacheEvict/learning/IncrementalLearn.java
+++ src/main/java/com/tjut/cacheEvict/learning/IncrementalLearn.java
@@ -1,8 +1,13 @@
 package com.tjut.cacheEvict.learning;
 
+import com.tjut.cacheEvict.config.Config;
 import com.tjut.cacheEvict.sample.AbstractSampleLib;
 import com.tjut.cacheEvict.sample.ILSampleLib;
+import com.yahoo.labs.samoa.instances.Attribute;
+import com.yahoo.labs.samoa.instances.Instance;
+import com.yahoo.labs.samoa.instances.Instances;
 import moa.classifiers.meta.LearnNSE;
+import moa.streams.CachedInstancesStream;
 
 /**
  * @author tb
@@ -12,6 +17,8 @@ public class IncrementalLearn {
 
     LearnNSE learnNSE;
     static IncrementalLearn incrementalLearn = new IncrementalLearn();
+    static Instances instances;
+    static CachedInstancesStream  cachedInstancesStream;
 
     public static IncrementalLearn getInstance() {
         return incrementalLearn;
@@ -20,13 +27,20 @@ public class IncrementalLearn {
     private IncrementalLearn() {
         learnNSE = new LearnNSE();
         learnNSE.resetLearningImpl();
+        Attribute[] attribute;
+        =  new Attribute();
+        instances = new Instances("ILCache", attribute, Config.getInstance().getTrainingInterval());
+        instances.setClassIndex(Config.getInstance().getFeatureNum());//index start from 0, so last is label
+//        instances.setAttributes();
+//        instances. = new ArrayList<Instance>();
+        this.computeAttributesIndices();
+
+        cachedInstancesStream = new CachedInstancesStream(instances);
     }
 
     public static int evict(int[] evictCandidates){
         int evictedSize = 0;
         int[] evictedObj = getEvictedObjsByCandidates(evictCandidates);
-
-
         ILSampleLib.addSubscribedObj(evictedObj);//subscribe evicted obj
         return evictedSize;
     }
@@ -34,7 +48,7 @@ public class IncrementalLearn {
     private static int[] getEvictedObjsByCandidates(int[] evictCandidates) {
         int[] evictedObj = null;
 
-
+        getVotesForInstance(Instance inst)
         return evictedObj;
     }
 
@@ -42,6 +56,6 @@ public class IncrementalLearn {
 
     }
     public static void selfUpdate(){
-
+        trainOnInstanceImpl(Instance inst)
     }
 }
